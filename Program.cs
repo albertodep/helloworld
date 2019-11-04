2696ada7f59c22852d7f2c7079cd675f07374593
albertodep <57347317+albertodep@users.noreply.github.com>
2019-11-04 14:14:01 +0100

    commit 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Model;
+using System;
 
 namespace HelloWorld
 {
@@ -6,7 +7,36 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Value Type  memorizzati nello stack
+            int pippo = 3;
+            var pippo2 = 2;
+            //char carattere = 'j';
+            //decimal valoreDecimale(); //valori monetari
+
+            //Reference Type memorizzati nell'heap
+            //string stringa;
+            //string stringa2;
+            //object oggetto;
+           
+            Person luca = new Person("Luca", "Lippi");
+            Person Giovanni = new Person("Luca", "Lippi");
+            //p.eta = 18;
+
+            Console.WriteLine(luca.Anagrafica());
+            Giovanni = luca;
+
+            Giovanni.nome = "Giovanni";
+
+
+            //stringa = 'la mia casa è bella';
+            //stringa2 = stringa1;
+
+            //Console.WriteLine(stringa);
+            //Console.WriteLine(string2);
+
+
+
+
         }
     }
 }
